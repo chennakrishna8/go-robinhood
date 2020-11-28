e3a88a2c14aaa7c912a5f1f0b10ac24eeea592d9
Chennakrishna Sagili <csagili@cmsonline.com>
2020-11-28 16:39:57 -0700

    fixing package name

--- cryptoHistoricals.go
+++ cryptoHistoricals.go
@@ -1,18 +1,20 @@
-package robihood
+package robinhood
 
 import "fmt"
 
+// Historical data represents class ohlc data i.e open, high, low, close
 type Historical struct {
-	BeginsAt     time.Time `json:"begins_at"`
-	OpenPrice    float64    `json:"open_price,string"`
-	ClosePrice   float64    `json:"close_price,string"`
-	HighPrice    float64    `json:"high_price,string"`
-	LowPrice     float64    `json:"low_price,string"`
-	Volume       int       `json:"volume"`
-	Session      string    `json:"session"`
-	Interpolated bool      `json:"interpolated"`
+	BeginsAt     string  `json:"begins_at"`
+	OpenPrice    float64 `json:"open_price,string"`
+	ClosePrice   float64 `json:"close_price,string"`
+	HighPrice    float64 `json:"high_price,string"`
+	LowPrice     float64 `json:"low_price,string"`
+	Volume       int     `json:"volume"`
+	Session      string  `json:"session"`
+	Interpolated bool    `json:"interpolated"`
 }
 
+// GetDailyHistoricals will give daily high, low, ope, close data for the given symbol
 func (c *Client) GetDailyHistoricals(cryptoID string) ([]Historical, error) {
 	url := EPMarket + fmt.Sprintf("forex/historicals/${0}", cryptoID) + "/?bound=24_7&interval=day&span=week"
 	var r struct{ Results []Historical }
